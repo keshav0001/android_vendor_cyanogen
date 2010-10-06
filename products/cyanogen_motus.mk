6452be426672ab9261b4aed90c1de1e6b1d30ef1
Patrick Jacques <kernelzilla@kinetic-computing.com>
2010-10-05 21:18:53 -0700

    morus: update build id
    Use FRG83 (2.2.1) build-id
    various cleanup

--- products/cyanogen_motus.mk
+++ products/cyanogen_motus.mk
@@ -1,5 +1,5 @@
 # Inherit device configuration for motus.
-$(call inherit-product, device/motorola/motus/full_motus.mk)
+$(call inherit-product, device/motorola/motus/motus.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common.mk)
@@ -12,12 +12,9 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := motus
 PRODUCT_MODEL := MB300
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2 FRF91 43546 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-motus.map
-
-# Build kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL := device/motorola/motus/kernel
 
 # Extra Motus overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/motus
@@ -30,7 +27,7 @@ PRODUCT_PACKAGES += Torch
 #
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Motus
+	ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Motus
 else
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.modversion=CyanogenMod-6.1.0-Motus-alpha1
