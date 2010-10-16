8b10d9bb8168b906512fb2614c91188dbf919309
Keshav Gupta <keshav0001@gmail.com>
2010-10-17 01:33:36 +0530

    support motus
    Change-Id: I5b84c68a31368039b2164c23d32d860ad625b14a

---
+++ products/cyanogen_motus.mk
@@ -0,0 +1,42 @@
+# Inherit device configuration for motus.
+$(call inherit-product, device/motorola/motus/motus.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_motus
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := motus
+PRODUCT_MODEL := MB300
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-motus.map
+
+# Extra Motus overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/motus
+
+# Include the Torch and FM apps
+PRODUCT_PACKAGES += \
+    Torch \
+    FM
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+	ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Motus
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-6.1.0-Motus-alpha1
+endif
+
+
+#
+# Copy Motus specific prebuilt files
+#
+PRODUCT_COPY_FILES += vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
